33b9af24984470c0ea664f9808f824390f140c0f
kkulhavi <78230187+kkulhavi@users.noreply.github.com>
2022-06-06 10:24:21 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'host': 'us-cdbr-east-05.cleardb.net',
+        'user': 'b489bbeb692707',
+        'password': '2093ff03'
     },
-	'database': 'test2',
+	'database': 'heroku_1e4babdd933339d',
     'users_table': 'users'
 };
